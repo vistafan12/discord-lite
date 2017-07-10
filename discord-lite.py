2936ed2989b8e441cb76897d49e93232d0902c04
vistafan12 <kroljestboss@o2.pl>
2017-07-10 10:46:27 +0200

    Canary build <-> Normal build

--- discord-lite.py
+++ discord-lite.py
@@ -5,6 +5,7 @@ from PySide.QtWebKit import QWebView
 
 app = QApplication(sys.argv)
 b = QWebView()
-b.load(QUrl('https://canary.discordapp.com/login'))
+# b.load(QUrl('https://canary.discordapp.com/login'))
+b.load(QUrl('https://discordapp.com/login'))
 b.show()
 app.exec_()
