02a600d1ffcb3ea57c44a7ed21ee348ec39ad250
vistafan12 <kroljestboss@o2.pl>
2017-07-10 10:30:44 +0200

    ⬆️ 0.0.1 indev ⬆️

---
+++ discord-lite.py
@@ -0,0 +1,10 @@
+import sys	
+from PySide.QtGui import QApplication
+from PySide.QtCore import QUrl
+from PySide.QtWebKit import QWebView
+
+app = QApplication(sys.argv)
+b = QWebView()
+b.load(QUrl('https://canary.discordapp.com/login'))
+b.show()
+app.exec_()
